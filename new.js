c625fa4d1bef2497dbe15b01de44a520174a680f
pradeep gururani <gururanipankaj5@gmail.com>
2019-03-12 16:56:32 +0530

    new log

--- new.js
+++ new.js
@@ -1,8 +1,9 @@
 var arr = ['A','B','C','D','E'];
 for( var k in arr ){
-	console.log( arr[k] ); 
+	console.log( arr[k] ); //logs con
 }
 
 var jsond = require('./config.json');
 console.log(jsond.username);
 console.log(jsond.password);
+console.log('new entry');
